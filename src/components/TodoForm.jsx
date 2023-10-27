65a170766b0b4e5ca1e844b72a1f34d500da641c
Roja kundar <kundarroja@gmail.com>
2023-10-27 12:57:13 +0530

    Updated the form style

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -19,11 +19,11 @@ return (
       <input
           type="text"
           placeholder="Write Todo..."
-          className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+          className="w-full border border-black/10 rounded-l-2xl px-3 outline-none duration-150 bg-white/80 py-1.5 text-black/100"
            value={todo}
            onChange={(e) => setTodo(e.target.value)}      
       />
-      <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
+      <button type="submit" className="rounded-tr-2xl px-3 py-1 bg-green-600 text-white shrink-0">
           Add
       </button>
   </form>
